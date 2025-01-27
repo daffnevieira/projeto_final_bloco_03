b8cef68df888bd70249c80c0340b7c5d3f25f8ce
Seu Nome <seuemail@exemplo.com>
2025-01-27 15:24:04 -0300

    03_Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import Footer from './components/footer/Footer'
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import Categorias from './pages/categorias/Categorias'
 import Home from './pages/home/Home'
+import ListaCategorias from './components/categorias/ListaCategorias'
+import FormCategorias from './components/categorias/FormCategorias'
+import DeleteCategorias from './components/categorias/DeleteCategorias'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,9 +19,14 @@ function App() {
           <Navbar />
           <div className="min-h-80vh]">
             <Routes>
-              {/* <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} /> */}
-              <Route path="/categorias" element={<Categorias />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/listarcategorias" element={<ListaCategorias />} />
+              <Route path="/cadastrarcategorias" element={<FormCategorias />} />
+              <Route path="/editarcategorias" element={<FormCategorias />} />
+              <Route path="/deletarcategorias/:id" element={<DeleteCategorias categoriaId={0} onCategoriaDeletada={function (): void {
+              throw new Error('Function not implemented.')
+            } } />} />
             </Routes>
           </div>
           <Footer />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,15 +5,17 @@ function Navbar() {
   return (
     <div className="w-full  bg-green-200 text-gray-900 flex justify-center py-4">
       <div className="container flex justify-between text-lg ">
-        <div className="text-2xl font-bold">Farmácia SaúdeMil</div>
-        {/* <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link> */}
+        {/* <div className="text-2xl font-bold">Farmácia SaúdeMil</div> */}
+        <Link to='/home' className="text-2xl font-bold">Farmácia SaúdeMil</Link>
         <div className="flex gap-4">
           Produtos
 
           <Link to='/categorias' className='hover:underline'>Categoria</Link>
           {/* <Link to='/categorias' className='hover:underline'>Postagens</Link>
           <Link to='/produtos' className='hover:underline'>Temas</Link> */}
-          
+          <Link to='/cadastrarcategorias'className='hover:underline'>Cadastrar Categorias</Link>
+          <Link to='/editarcategorias'className='hover:underline'>Editar Categorias</Link>
+          <Link to='deletarcategorias' className='hover:underline'>Deletar Categoria</Link>
         </div>
       </div>
     </div>
